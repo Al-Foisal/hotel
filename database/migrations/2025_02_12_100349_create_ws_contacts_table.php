ae8d7c094bcb4f0f261243cb778fce4f88524cee
Md Hafiz Al foisal <alfoisal940700@gmail.com>
2025-02-12 16:50:31 +0600

    website backend running

---
+++ database/migrations/2025_02_12_100349_create_ws_contacts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ws_contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone');
+            $table->string('subject');
+            $table->string('message')->nullable();
+            $table->tinyInteger('is_responce')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ws_contacts');
+    }
+};
