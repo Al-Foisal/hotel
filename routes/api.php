55c202e817ff865a3105617c95a793fa653af49b
Md Hafiz Al foisal <alfoisal940700@gmail.com>
2025-02-19 11:33:31 +0600

    api added for website setup

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\WsAbout;
+use App\Models\WsSetup;
+use App\Models\WsTestimonial;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/ws-about', function () {
+    return WsAbout::where('status', 1)->get();
+});
+Route::get('/ws-testimonial', function () {
+    return WsTestimonial::where('status', 1)->get();
+});
+Route::get('/ws-setup', function () {
+    return WsSetup::find(1);
+});
